89cc68b3be05026af19e9e4a01a5689a9dacbc55
simnavs <sonigasy2021@gmail.com>
2025-07-27 13:18:13 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use("/stream", createProxyMiddleware({
+  target: "https://radio2.pro-fhi.net",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/stream": "/listen-ddrzvfve-stream.mp3"
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+  }
+}));
+
+app.listen(10000, () => {
+  console.log("Proxy is running on port 10000");
+});
